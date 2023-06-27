89a12c491f6a9e6adba98064e2545d4e78e6deba
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-06-27 13:45:09 -0400

    setting and config

--- pract/project/app/views.py
+++ pract/project/app/views.py
@@ -6,3 +6,15 @@ from .serializer import *
 
 
 # Create your views here.
+
+class ReactView(APIView):
+    def get(self, request):
+        output = [{"employee": output.employee, "department":output.department}
+        for output in React.objects.all()]
+        return Response(output)
+        
+    def post(self, request):
+        serializer = ReaactSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
